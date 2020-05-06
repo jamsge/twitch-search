0ef86d8d2193509c4dea51dec7f8486e47587451
James Ge <james.ge1@outlook.com>
2020-05-05 18:51:29 -0700

    changed column width for userinfo

--- frontend/src/components/UserInfo.vue
+++ frontend/src/components/UserInfo.vue
@@ -1,8 +1,8 @@
 <template>
   <b-container>
     <b-row align-h="between">
-      <b-col class="text-left" cols="8"><h5>{{username}}</h5></b-col>
-      <b-col v-if="followers != -1" class="text-right" cols="4">
+      <b-col class="text-left" cols="6"><h5>{{username}}</h5></b-col>
+      <b-col v-if="followers != -1" class="text-right" cols="6">
         <p class="h5 mb-2"><b-icon icon="heart-fill"></b-icon> {{followers}}</p>
       </b-col>
     </b-row>
